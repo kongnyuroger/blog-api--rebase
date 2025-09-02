94b5a7c72793b4e9cb31bcf8f5b1c6c7f3c97e91
Kongnyu Roger <kongnyuroger711@gmail.com>
2025-09-02 10:33:10 +0100

    generated project with express generator

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import createError from 'http-errors'; 
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan';
+import express from 'express';
+
+
+
+import indexRouter  from './routes/index.js';
+import usersRouter from  './routes/users.js';
+
+const app = express();
+
+// view engine setup
+
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 5000);
+  res.render('error');
+});
+
+export default app;

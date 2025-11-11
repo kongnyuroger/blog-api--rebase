d8120e5fde0b64db11d534e4d7dd97f41f1ffbfd
Kongnyu Roger <kongnyuroger711@gmail.com>
2025-11-11 14:42:04 +0100

    fixed the query to get all post

--- src/modules/post/post.model.js
+++ src/modules/post/post.model.js
@@ -41,8 +41,8 @@ export async function findPost(id){
 }
 
 export async function listPosts({limit, offset}){
-    const result = await pool.query(`select * from posts limit $1 offset $2 order by 
-                                created_at` ,
+    const result = await pool.query(`SELECT * FROM posts ORDER BY created_at LIMIT $1 OFFSET $2;
+` ,
                                 [limit, offset]
                                 );
     return result.rows
